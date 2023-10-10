4e76b67b222500c5654a51e89f60f242eada8afb
Jules Sorensen <julessorens@gmail.com>
2023-10-10 14:01:04 +0200

    edt api requests

--- src/api/enigma.ts
+++ src/api/enigma.ts
@@ -1,248 +1,140 @@
 import axios from "axios";
-import * as sha1 from "sha1";
-import { encrypt, decrypt } from "../tasks/crypter.js";
-import { getAllData, createData } from "../firebase/firebase.js";
-import { getCurrentDate } from "../tasks/dates.js";
+import * as XLSX from 'xlsx';
+// import * as sha1 from "sha1";
+// import { encrypt, decrypt } from "../tasks/crypter.js";
+// import { getAllData, createData } from "../firebase/firebase.js";
+// import { getCurrentDate } from "../tasks/dates.js";
 
-const mgLogin = async (username, password) => {
-  const token = await mgGenerateAccessToken(username, password);
-  if (!token) {
-    throw new Error("Bad credentials");
-  }
-  return token;
-};
-const mgGenerateAccessToken = async (username, password) => {
-  var _a, _b, _c;
-  try {
-    const credentials = Buffer.from(`${username}:${password}`, "utf8").toString(
-      "base64"
-    );
-    await axios.get(
-      "https://authentication.kordis.fr/oauth/authorize?response_type=token&client_id=skolae-app",
-      {
-        headers: {
-          Authorization: `Basic ${credentials}`,
-        },
-        maxRedirects: 0,
-      }
-    );
-    return null;
-  } catch (e: any) {
-    if (
-      !((_c =
-        (_b = (_a = e.request) === null || _a === void 0 ? void 0 : _a.res) ===
-          null || _b === void 0
-          ? void 0
-          : _b.headers) === null || _c === void 0
-        ? void 0
-        : _c.location)
-    ) {
-      throw new Error("Bad password");
+enum ECursus {
+  RETAIL = "RETAIL",
+  CYBER = "CYBER",
+}
+
+interface IEdtInfo {
+  id: string
+  name: string
+  lastModifiedDateTime: string
+  lastModifiedBy: {
+    user: {
+      displayName: string
+      email: string
     }
-    const { location } = e.request.res.headers;
-    const hash = location.slice(location.indexOf("#") + 1);
-    const properties = hash
-      .split("&")
-      .map((property) => property.split("="))
-      .reduce(
-        (acc, [name, value]) =>
-          Object.assign(Object.assign({}, acc), { [name]: value }),
-        {}
-      );
-    return {
-      access_token: properties.access_token,
-      token_type: properties.token_type,
-      expires_in: properties.expires_in,
-      scope: properties.scope,
-      uid: properties.uid,
-    };
   }
-};
+}
 
-const getToken = async (force = false) => {
-  const generateNewToken = async () => {
-    const tokenRes = await mgLogin(
-      process.env.MYGES_LOGIN,
-      process.env.MYGES_PASSWORD
-    );
-    const exp = Math.floor(
-      new Date().getTime() / 1000 + parseInt(tokenRes.expires_in)
-    );
+interface IEdtContent {
+  date: Date
+  day: string
+  morning: string
+  afternoon: string
+}
 
-    return { token: tokenRes.access_token, exp: exp };
+let msRefreshToken: string = process.env.MS_REFRESH_TOKEN;
+let msAccessToken: string | undefined;
+
+const getAccessToken = async (): Promise<string> => {
+  const url = `https://login.microsoftonline.com/${process.env.MS_APP_ID}/oauth2/v2.0/token`;
+  const data = {
+    grant_type: "refresh_token",
+    refresh_token: msRefreshToken,
+    client_id: process.env.MS_APP_CLIENT_ID,
+    client_secret: process.env.MS_APP_CLIENT_SECRET
   };
+  const response = await axios.post(url, data);
 
-  const mgDatas = await getAllData("myges");
-  let regenerateToken = true;
-  let token;
-  if (mgDatas?.data?.token && !force) {
-    try {
-      const tokenDatas = JSON.parse(decrypt(mgDatas.data));
-      const curTS = Math.floor(new Date().getTime() / 1000 - 600);
-      if ((tokenDatas.token && tokenDatas.exp) || curTS < tokenDatas.exp) {
-        regenerateToken = false;
-        token = tokenDatas.token;
-      }
-    } catch (e) { }
-  }
+  msAccessToken = response.data.access_token;
 
-  if (regenerateToken) {
-    const tokenDatas = await generateNewToken();
-    const encryptedData = encrypt(JSON.stringify(tokenDatas));
-    await createData("myges", "data", { token: encryptedData });
-    token = tokenDatas.token;
-  }
-
-  return token;
+  return msAccessToken;
 };
 
-const getNotesCrypted = async () => {
-  const token = await getToken();
-  let grades;
-  try {
-    grades = await axios.get(`https://api.kordis.fr/me/2022/grades`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  } catch (error: any) {
-    if (error?.response?.data?.error === "invalid_token") {
-      grades = await axios.get(`https://api.kordis.fr/me/2022/grades`, {
-        headers: { Authorization: `Bearer ${await getToken(true)}` },
-      });
-    } else throw error;
+const isTokenValid = (): boolean => {
+  // decode and check exp value
+  if (!msAccessToken) return false;
+}
+
+const getEdtInfo = async (cur: ECursus): Promise<Array<Date | string>> => {
+  let fileId: string;
+  switch (cur) {
+    case ECursus.RETAIL:
+      fileId = process.env.MS_EXCEL_RETAIL_ID;
+      break;
+    default:
+      fileId = process.env.MS_EXCEL_CYBER_ID;
+      break;
   }
-  return sha1(
-    grades?.data === "" ? "UNDEFINED" : JSON.stringify(grades.data.result)
-  );
+
+  const url = `https://graph.microsoft.com/v1.0/me/drives/${process.env.MS_DRIVE_GROUP_ID}/items/${fileId}`;
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${msAccessToken}`
+    }
+  });
+
+  return response.data.value;
 };
 
-const getAgendaCrypted = async (dates, withDetails = false) => {
-  const { start, end } = {
-    start: getCurrentDate(dates.start, "YYYY-MM-DD").toDate().valueOf(),
-    end: getCurrentDate(dates.end, "YYYY-MM-DD").toDate().valueOf(),
-  };
-  const token = await getToken();
-  let agenda;
-  try {
-    agenda = await axios.get(
-      `https://api.kordis.fr/me/agenda?start=${start}&end=${end}`,
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-  } catch (error: any) {
-    if (error?.response?.data?.error === "invalid_token") {
-      agenda = await axios.get(
-        `https://api.kordis.fr/me/agenda?start=${start}&end=${end}`,
-        { headers: { Authorization: `Bearer ${await getToken(true)}` } }
-      );
-    } else throw error;
+const translateFieldName = (fieldName: string): string => {
+  switch (fieldName.toLowerCase()) {
+    case "jour":
+      return "jour";
+    case "matin":
+      return "morning";
+    case "apres-midi":
+    case "après-midi":
+    case "apres midi":
+    case "après midi":
+      return "afternoon";
+    default:
+      return fieldName;
+  }
+}
+
+const getEdt = async (cur: ECursus): Promise<IEdtContent[]> => {
+  let fileId: string;
+  switch (cur) {
+    case ECursus.RETAIL:
+      fileId = process.env.MS_EXCEL_RETAIL_ID;
+      break;
+    default:
+      fileId = process.env.MS_EXCEL_CYBER_ID;
+      break;
   }
 
-  let details = ``;
-  if (withDetails) {
-    const jours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
-    let detailsPerDate = {};
+  const url = `https://graph.microsoft.com/v1.0/me/drives/${process.env.MS_DRIVE_GROUP_ID}/items/${fileId}/content`;
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${msAccessToken}`,
+    },
+    responseType: "arraybuffer"
+  });
 
-    agenda.data.result.forEach((event) => {
-      const currentDate = jours[getCurrentDate(new Date(event.start_date)).day() - 1];
-      const matinOuAprem = getCurrentDate(new Date(event.start_date)).hour() < 13 ? "am" : "pm";
-      if (!detailsPerDate[currentDate]) {
-        detailsPerDate[currentDate] = {};
-        detailsPerDate[currentDate]["am"] = [];
-        detailsPerDate[currentDate]["pm"] = [];
-      }
+  const workbook = XLSX.read(response.data, { type: 'buffer' });
 
-      let eventName = event.name;
-      if (event.comment) {
-        eventName += ` (*${event.comment}*)`;
-      }
-      if (event.discipline.teacher_id) {
-        eventName += ` [__${event.discipline.teacher}__]`;
-      }
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
 
-      if (!detailsPerDate[currentDate][matinOuAprem].includes(eventName)) {
-        detailsPerDate[currentDate][matinOuAprem].push(eventName);
-      }
-    });
+  const rawDatas: any[][] = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
 
-    // details
-    const sortedDetailsPerDate = {};
-    Object.keys(detailsPerDate)
-      .sort((a, b) => jours.indexOf(a) - jours.indexOf(b))
-      .forEach((key) => {
-        sortedDetailsPerDate[key] = detailsPerDate[key];
-      });
-
-    Object.keys(sortedDetailsPerDate).forEach((day) => {
-      details += `__**${day}**__\\n`;
-      details += `   **Matin :** ${detailsPerDate[day]["am"].length > 0 ? detailsPerDate[day]["am"].join(" / ") : "aucun cours"}\\n`;
-      details += `   **Après-midi :** ${detailsPerDate[day]["pm"].length > 0 ? detailsPerDate[day]["pm"].join(" / ") : "aucun cours"}\\n`;
-    });
-
-    setRemind(agenda);
-    setSalles({ agenda: agenda });
-
-    return {
-      cryptedAgenda: sha1(JSON.stringify(agenda?.data?.result)),
-      details: details
+  const header = rawDatas[1];
+  const objectDatas = rawDatas.slice(2).map((row) => {
+    const obj: IEdtContent = {
+      date: undefined,
+      day: undefined,
+      morning: undefined,
+      afternoon: undefined
     };
-  }
-  return sha1(JSON.stringify(agenda?.data?.result));
-};
 
-const setRemind = async (agenda) => {
-  const startHourPerDate = {};
-  agenda?.data?.result.map((event) => {
-    const currentDate = getCurrentDate(new Date(event.start_date)).format("YYYY-MM-DD");
-    const currentHour = getCurrentDate(new Date(event.start_date)).format("H[h]mm");
-
-    if (!startHourPerDate[currentDate]) {
-      startHourPerDate[currentDate] = currentHour;
-    } else if (getCurrentDate(currentHour, "H[h]mm").isBefore(getCurrentDate(startHourPerDate[currentDate], "H[h]mm"))) {
-      startHourPerDate[currentDate] = currentHour;
-    }
+    row.forEach((value: any, i: number) => {
+      while (!header[i] && i > 0) { i--; }
+      const fieldName = translateFieldName(header[i]);
+      if (fieldName === "date") {
+        obj[fieldName] = new Date((value - 2) * 24 * 3600 * 1000 + Date.parse('1900-01-01'));
+      } else {
+        obj[translateFieldName(header[i])] = value;
+      }
+    });
+    return obj;
   });
 
-  Object.keys(startHourPerDate).forEach(async (date) => {
-    await createData("edtremind", date, { "hour": startHourPerDate[date] });
-  });
+  return objectDatas;
 };
-
-const setSalles = async (params) => {
-  if (!params.agenda) {
-    const token = await getToken();
-    try {
-      params.agenda = await axios.get(
-        `https://api.kordis.fr/me/agenda?start=${params.start}&end=${params.end}`,
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-    } catch (error: any) {
-      if (error?.response?.data?.error === "invalid_token") {
-        params.agenda = await axios.get(
-          `https://api.kordis.fr/me/agenda?start=${params.start}&end=${params.end}`,
-          { headers: { Authorization: `Bearer ${await getToken(true)}` } }
-        );
-      } else throw error;
-    }
-  }
-
-  const salles = {};
-  params.agenda?.data?.result.map((event) => {
-    const datetime = getCurrentDate(new Date(event.start_date));
-    const date = datetime.format("YYYY-MM-DD");
-    const time = datetime.format("H[h]mm");
-
-    if (!salles[date]) { salles[date] = {}; }
-
-    salles[date][event.start_date] = `**${time}** - ${event.rooms ? event.rooms.length > 1 ? "Salles" : "Salle" : "Aucune salle définie"} ${event.rooms ? event.rooms.map((room) => `**__${room.name}__** **${room.floor}**`).join(" / ") : ''} ${event.name ? `(*${event.name}*)` : ''}`;
-  });
-
-  await (async (array, callback) => {
-    for (let index = 0; index < array.length; index++) {
-      await callback(array[index])
-    }
-  })(Object.keys(salles), async (date) => {
-    await createData("salles", date, salles[date]);
-  })
-  return;
-};
-
-module.exports = { getNotesCrypted, getAgendaCrypted, setSalles };
