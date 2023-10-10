1f527daf196bb4b3a809cb8af1dcd88c53c668f4
Jules Sorensen <julessorens@gmail.com>
2023-10-10 19:42:37 +0200

    small changes in main file

--- main.ts
+++ main.ts
@@ -3,15 +3,6 @@ import { Client, Events, GatewayIntentBits, Partials, PresenceStatusData, TextCh
 import config from "./src/config/config.json";
 import { setAllCommands } from "./src/tasks/setAllCommands";
 import { interactionLaunch } from "./src/tasks/commandLauch";
-const version = config.version;
-
-const authServers: string[] = [
-	process.env.GLD_ENIGMA,
-	"783679631101526056",
-	process.env.GLD_ADMIN,
-];
-const bannedUsers: string[] = [];
-let uptime = new Date();
 
 const client = new Client({
 	partials: [
@@ -37,6 +28,42 @@ const client = new Client({
 	]
 });
 
+const authServers: string[] = [
+	process.env.GLD_ENIGMA,
+	process.env.GLD_ADMIN
+];
+const uptime = new Date();
+const version = config.version;
+
+client.on(Events.ClientReady, () => {
+	console.log(`-------------------------\nLogged in as ${client.user.username} !\nVersion: ` + version + ` ✅\n-------------------------\n`);
+
+	client.user.setStatus(process.env.BOT_STATUS as PresenceStatusData);
+
+	// TODO Specific to Enigma
+	// edtCheck(client, min); // min = 5, check the edt status every 5 minutes
+
+	// TODO For development state, remove those lines
+	// setAllCommands(process.env.GLD_ENIGMA, client); // enigma
+	// setAllCommands(process.env.GLD_ADMIN, client); // private server
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.user.bot) await interactionLaunch(interaction, client, version, new Date(), uptime);
+});
+
+client.on(Events.MessageReactionAdd, (reaction, user) => {
+	// if (!interaction.user.bot) reactionAdded(reaction, user);
+});
+
+client.on(Events.MessageReactionRemove, (reaction, user) => {
+	// if (!interaction.user.bot) reactionRemoved(reaction, user);
+});
+
+client.on(Events.GuildCreate, (guild) => {
+	if (authServers.includes(guild.id)) setAllCommands(guild.id, client);
+});
+
 process.on("uncaughtException", function (err) {
 	console.log("/!\\ GERROR :", err);
 	const channel = client.channels.cache.get(process.env.CHNL_ERROR)
@@ -45,47 +72,4 @@ process.on("uncaughtException", function (err) {
 	}
 });
 
-client.on(Events.ClientReady, () => {
-	console.log(
-		`-------------------------\nLogged in as ${client.user.username} !\nVersion: ` +
-		version +
-		` ✅\n-------------------------\n`
-	);
-
-	client.user.setStatus(process.env.BOT_STATUS as PresenceStatusData);
-
-	// TODO Specific to Efficom
-	// edtReminderCheck(client);
-	// edtSenderCheck(client);
-	// mygesCheck(client);
-	// currentNotesCheck(client);
-
-	// setAllCommands(process.env.GLD_ENIGMA, client); // enigma
-	// setAllCommands(process.env.GLD_ADMIN, client); // private server
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-	if (!bannedUsers.includes(interaction?.user?.id)) {
-		await interactionLaunch(interaction, client, version, new Date(), uptime);
-	}
-});
-
-client.on(Events.MessageReactionAdd, (reaction, user) => {
-	if (!bannedUsers.includes(user.id)) {
-		// reactionAdded(reaction, user);
-	}
-});
-
-client.on(Events.MessageReactionRemove, (reaction, user) => {
-	if (!bannedUsers.includes(user.id)) {
-		// reactionRemoved(reaction, user);
-	}
-});
-
-client.on(Events.GuildCreate, (guild) => {
-	if (authServers.includes(guild.id)) {
-		setAllCommands(guild.id, client);
-	}
-});
-
-client.login(process.env["TOKEN"]);
+client.login(process.env.TOKEN);
