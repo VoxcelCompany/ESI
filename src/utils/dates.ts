7dd42cc34d91f41e6789cdfbc1aea5e55b650829
Guillaume K. <kesteman.guillaume@gmail.com>
2023-10-12 10:33:11 +0200

    perf: optimize getMomentDate()

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,7 +1,5 @@
 import moment from "moment-timezone";
 
 export const getMomentDate = (date?: string | Date, format?: string) => {
-    return date === undefined ?
-        moment().tz("Europe/Paris") : format === undefined ?
-            moment(date).tz("Europe/Paris") : moment(date, format).tz("Europe/Paris");
+    return moment(date, format).tz("Europe/Paris");
 };
