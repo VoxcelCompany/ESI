e34ea5eb0667eddca103293d128ef15b3acc0019
Jules <38363471+JulesSorensen@users.noreply.github.com>
2023-10-30 15:06:11 +0100

    Feat/signalert (#46)
    * fix: sent date + get monday date
    
    * feat: sign mail alert
    
    * fix: rename env variable CHNL_SIGN

---
+++ src/service/mail.service.ts
@@ -0,0 +1,50 @@
+import { Client, TextChannel } from "discord.js";
+import MailServiceData from "../models/MailServiceData";
+import mailRepository from "../repository/mail.repository";
+import { getMomentDate } from "../utils/dates";
+
+class MailService {
+    private data: MailServiceData = {
+        date: 0,
+        morning: false,
+        afternoon: false,
+    };
+
+    async sendMailWarning(client: Client): Promise<void> {
+        const currentDate = getMomentDate();
+
+        const isMorning = currentDate.hour() < 12 || (currentDate.hour() == 12 && currentDate.minute() < 30);
+        const isAfternoon = currentDate.hour() > 14 || (currentDate.hour() == 14 && currentDate.minute() >= 0);
+
+        if (
+            (!isMorning && !isAfternoon) ||
+            (isMorning && this.data.date == currentDate.date() && this.data.morning) ||
+            (isAfternoon && this.data.date == currentDate.date() && this.data.afternoon)
+        )
+            return;
+
+        const lastMail = await mailRepository.getLastMailFrom("noreply@sowesign.net");
+        const mailDate = getMomentDate(lastMail.createdDateTime);
+
+        if (
+            mailDate.date() != currentDate.date() ||
+            (isMorning && mailDate.hour() < 12) ||
+            (isAfternoon && mailDate.hour() > 14)
+        )
+            return;
+
+        this.data = {
+            date: currentDate.date(),
+            morning: this.data.date == currentDate.date() ? this.data.morning : isMorning,
+            afternoon: this.data.date == currentDate.date() ? this.data.afternoon : isAfternoon,
+        };
+
+        const channel = client.channels.cache.get(process.env.CHNL_SIGN) as TextChannel;
+
+        if (channel) {
+            await channel.send("📧 N'oubliez pas de signer !");
+        }
+    }
+}
+
+export default new MailService();
