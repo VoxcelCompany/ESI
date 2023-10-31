edc1a53677c33e3e0cd2d98b098bdf2a2928d085
Guillaume K. <kesteman.guillaume@gmail.com>
2023-10-31 12:06:21 +0100

    feat: separate menu action and menu slash command

---
+++ src/commands/slashCommands/menu.slashCommand.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder } from "discord.js";
+import MenuService from "../../service/menu.service";
+import { getMomentDate } from "../../utils/dates";
+
+export default async (): Promise<Omit<SlashCommandBuilder, "addSubcommand" | "addSubcommandGroup">> => {
+    const menusDays: Array<string> = await MenuService.getDays();
+    return new SlashCommandBuilder()
+        .setName('menu')
+        .setDescription('Affiche le menu du jour sélectionné')
+        .addStringOption(option => {
+                option = option.setName('jour')
+                    .setDescription('Jour concerné')
+                    .setRequired(true);
+
+                menusDays.forEach((day) => {
+                    console.log(day);
+                    option = option.addChoices({
+                        name: day,
+                        value: getMomentDate(day, "dddd D MMMM").format('L'),
+                    });
+                });
+
+                return option;
+            },
+        );
+}
